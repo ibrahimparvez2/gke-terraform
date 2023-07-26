32ba0b060a57ae5e7036106b5ca81c2e3beebbff
DE <ibrahimparvez2@yahoo.co.uk>
2023-07-26 21:47:43 +0100

    Minor update provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.29.0"
+    }
+  }
+}
+
 provider "google" {
-  credentials = file(var.credential_file_path)
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
 }
